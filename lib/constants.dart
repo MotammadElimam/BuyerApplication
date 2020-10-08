5e0edf5c995d8f40a2859d2ff4c2d3043d1755a7
DESKTOP-JI7NICL\Motamed <mo3tammad@gmail.com>
2020-10-08 23:59:38 +0200

    no message

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff0288D1);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFF0AACC1), Color(0xFF006064)],
+);
+
+const kSecondaryColor = Color(0xFFFFECDF);
+const kTextColor = Color(0xFF979797);
+const kAnamationDuration = Duration(milliseconds: 200);
+
+
