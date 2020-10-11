bd8c38a7dea5658717d9b1d9e46616b9e23c3121
DESKTOP-JI7NICL\Motamed <mo3tammad@gmail.com>
2020-10-11 16:25:04 +0200

    no message

--- lib/size_config.dart
+++ lib/size_config.dart
@@ -12,5 +12,19 @@ class SizeConfig{
     screenWidth = _mediaQueryData.size.width;
     screenHeight = _mediaQueryData.size.height;
     orientation = _mediaQueryData.orientation;
+
+    //get the proportionate height as per screen size
+    double getProportionateScreenHieght(double intputHeight){
+      double screenHeight = SizeConfig.screenHeight;
+      //812 is the layout height that designer use
+      return(intputHeight/812.0)*screenHeight;
+    }
+
+    //get the proportionate height as per screen size
+    double getProportionateScreenWidth (double inputwidth){
+      double screenWidth = SizeConfig.screenWidth;
+      //375 is the layout height that designer use
+      return (inputwidth/375.0)*screenWidth;
+    }
   }
 }
