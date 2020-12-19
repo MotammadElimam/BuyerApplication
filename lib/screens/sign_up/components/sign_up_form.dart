3dd6a0525abbbbdc734648e5781b4c7ebb49d400
DESKTOP-JI7NICL\Motamed <mo3tammad@gmail.com>
2020-12-19 17:52:32 +0200

    no message

--- lib/screens/sign_up/components/sign_up_form.dart
+++ lib/screens/sign_up/components/sign_up_form.dart
@@ -120,7 +120,7 @@ class _SignUpFormState extends State<SignUpForm> {
       },
       decoration: InputDecoration(
         labelText: "Password",
-        hintText: "Enter your passwordddddd",
+        hintText: "Enter your password",
         // If  you are using latest version of flutter then lable text and hint text shown like this
         // if you r using flutter less then 1.20.* then maybe this is not working properly
         floatingLabelBehavior: FloatingLabelBehavior.always,
