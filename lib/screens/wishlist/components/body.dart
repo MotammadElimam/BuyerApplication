b8b87fbec9e01451e696ed5b5f6be267b2ddc08b
DESKTOP-JI7NICL\Motamed <mo3tammad@gmail.com>
2020-12-22 13:55:22 +0200

    no message

--- lib/screens/wishlist/components/body.dart
+++ lib/screens/wishlist/components/body.dart
@@ -18,30 +18,32 @@ class _BodyState extends State<Body> {
     return Padding(
       padding:
           EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-      child: ListView.builder(
-        itemCount: wishlistbody.wishlist.wishlistitems.length,
-        itemBuilder: (context, index) => Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Dismissible(
-            key: Key(wishlistbody.wishlist.wishlistitems[index].product.id.toString()),
-            direction: DismissDirection.endToStart,
-            onDismissed: (direction) {
-                wishlistbody.removeFromWishlist(wishlistbody.wishlist.wishlistitems[index]);
-            },
-            background: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Color(0xFFFFE6E6),
-                borderRadius: BorderRadius.circular(15),
+      child: Expanded(
+              child: ListView.builder(
+          itemCount: wishlistbody.wishlist.wishlistitems.length,
+          itemBuilder: (context, index) => Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Dismissible(
+              key: Key(wishlistbody.wishlist.wishlistitems[index].product.id.toString()),
+              direction: DismissDirection.endToStart,
+              onDismissed: (direction) {
+                  wishlistbody.removeFromWishlist(wishlistbody.wishlist.wishlistitems[index]);
+              },
+              background: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFE6E6),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Row(
+                  children: [
+                    Spacer(),
+                    SvgPicture.asset("assets/icons/Trash.svg"),
+                  ],
+                ),
               ),
-              child: Row(
-                children: [
-                  Spacer(),
-                  SvgPicture.asset("assets/icons/Trash.svg"),
-                ],
-              ),
+              child: WishListCard(wishlistitem: wishlistbody.wishlist.wishlistitems[index]),
             ),
-            child: WishListCard(wishlistitem: wishlistbody.wishlist.wishlistitems[index]),
           ),
         ),
       ),
