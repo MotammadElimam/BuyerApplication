47633e5ce26a7ef1a146398021a4032cd910a87c
MOTAMMAD ELIMAM <mo3tammad@gmail.com>
2021-04-18 17:21:38 +0200

    no message

--- lib/controllers/databasehelper.dart
+++ lib/controllers/databasehelper.dart
@@ -6,10 +6,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 class DatabaseHelper {
   final String serverUrl = "http://192.168.43.92:8000";
   var status;
-
   var token;
 
-  loginData(String email, String password) async {
+ loginData(String email, String password) async {
     String logininformation = "email=" + "$email" + "&password=" + "$password";
 
     print(logininformation);
@@ -32,8 +31,6 @@ class DatabaseHelper {
       _save(data["token"]);
     }
   }
-
-
   registerData(
       String email,
       String password,
@@ -42,7 +39,6 @@ class DatabaseHelper {
       String lastName,
       String phoneNumber,
       //bool is_seller,
-
 
       String adress) async {
     Map information = {
@@ -73,17 +69,15 @@ class DatabaseHelper {
     }
   }
 
-  Future<List<dynamic>> getmyProducts() async{
-
+  Future<List<dynamic>> getmyProducts() async {
     final prefs = await SharedPreferences.getInstance();
     final key = 'token';
-    final value = prefs.get(key ) ?? 0;
+    final value = prefs.get(key) ?? 0;
 
     String myUrl = "$serverUrl/api/getallproducts/";
-    http.Response response = await http.get(myUrl,
-        headers: {
-          'Accept':'application/json',
-          'Authorization' : 'Bearer $value'
+    http.Response response = await http.get(myUrl, headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $value'
     });
     print(response.body);
     return jsonDecode(response.body);
@@ -105,60 +99,48 @@ class DatabaseHelper {
 //     });
 //   }*/
 
-// //   void getFileBase64(File image) {
-// //     final bytes = Io.File("").readAsBytesSync();
-// //   if (image != null) {
-// //     String img64 = base64Encode(bytes);
-// //     print(img64.substring(0, 100));
-// //   }
-// // }
 
- String getFileBase64(File image) {
-  if (image != null) {
-    List<int> bytes = image.readAsBytesSync();
-    String base64Image = base64Encode(bytes);
-    return base64Image;
+  String getFileBase64(File image) {
+    if (image != null) {
+      List<int> bytes = image.readAsBytesSync();
+      String base64Image = base64Encode(bytes);
+      return base64Image;
+    }
+    return null;
   }
-  return null;
-}
 
   void addData(String name, double price, String desc, File image) async {
     final prefs = await SharedPreferences.getInstance();
     final key = 'token';
     final value = prefs.get(key) ?? 0;
 
-
-     Map productdata = {
+    Map productdata = {
       "name": "$name",
       "price": "$price",
       "desc": "$desc",
       "image": getFileBase64(image),
-     };
-     print(productdata);
-
-
+    };
+    print(productdata);
 
     String myUrl = "$serverUrl/api/addproduct";
     final response = await http.post(myUrl,
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': 'Bearer $value'}, 
-      body: productdata );
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $value'
+        },
+        body: productdata);
 
-
-
-      print("hi ${response.statusCode}");
+    print("hi ${response.statusCode}");
 
     status = response.body.contains('error');
-      var data = json.decode(response.body);
+    var data = json.decode(response.body);
 
-      if (status) {
-        print('data : ${data["error"]}');
-      } else {
-        print('data : ${data["token"]}');
-        _save(data["token"]);
-      }
-  
+    if (status) {
+      print('data : ${data["error"]}');
+    } else {
+      print('data : ${data["token"]}');
+      _save(data["token"]);
+    }
   }
   // void editData(int id,String name , String price) async {
   //   final prefs = await SharedPreferences.getInstance();
@@ -179,13 +161,6 @@ class DatabaseHelper {
   //     print('Response body : ${response.body}');
   //   });
   // }
-
-
-  
-
-
-
-
 
   _save(String token) async {
     final prefs = await SharedPreferences.getInstance();
